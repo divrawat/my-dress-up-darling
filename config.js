36a2ebe5e578b86f19054c733fe92a6520a4a03e
divrawat <divrawat2001@gmail>
2024-06-19 23:01:34 +0530

    GIT YYUU

---
+++ config.js
@@ -0,0 +1,558 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.mydressupdarlingmanga.in.net';
+
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>My Dress-Up Darling</u></a> (Sono Bisque Doll wa Koi wo Suru) is an anime about Wakana Gojo, a shy high schooler who loves making hina dolls, and Marin Kitagawa, a popular girl who asks for his help with cosplay costumes. Their collaboration leads to a unique friendship and budding romance, as they explore their passions together.`
+
+
+export const MANGA_NAME = 'My Dress Up Darling';
+export const MANGA_AUTHOR = 'Shinichi Fukuda';
+export const MANGA_RELEASE = '2018';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Shinichi Fukuda';
+export const MANGA_STUDIO = 'CloverWorks';
+export const MANGA_GENRE = 'Romance, Drama, Comedy, Seinen, Adult, School';
+
+export const CHAPTER_PREFIX = 'my-dress-up-darling-chapter';
+export const IMAGE_PREFIX = 'images/my-dress-up-darling';
+export const NEXT_PREVIOUS_PREFIX = 'manga/my-dress-up-darling-chapter';
+export const URL_PREFIX = 'manga/my-dress-up-darling-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/my-dress-up-darling-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://shallowdivesinanime.wordpress.com/wp-content/uploads/2023/02/image-22.png?w=1024";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px]  rounded-full w-[80px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'My Dress Up Darling';
+export const NavbarName = "My Dress Up Darling";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Enjoy high-quality scans, latest chapters, and connect with fans in our vibrant community.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "My Dress-Up Darling (Sono Bisque Doll wa Koi wo Suru) is an anime that intricately weaves themes of passion, creativity, and romance through the lives of its main characters, Wakana Gojo and Marin Kitagawa."
+    },
+    {
+        id: 2,
+        content: "Wakana Gojo is a reserved high school student who has a deep-seated passion for creating hina dolls, a traditional Japanese art form. His love for this craft is rooted in his childhood, inspired by his grandfather who is a skilled hina doll maker. However, Wakana's devotion to this unusual hobby makes him an outsider among his peers, causing him to lead a solitary school life."
+    },
+    {
+        id: 3,
+        content: "Enter Marin Kitagawa, a lively and popular classmate whose outgoing personality starkly contrasts Wakana's introverted nature. Marin harbors a secret passion for cosplay, the art of dressing up as characters from anime, manga, and video games. Despite her enthusiasm, she lacks the sewing skills necessary to bring her favorite characters to life."
+    },
+    {
+        id: 4,
+        content: "Their worlds collide when Marin discovers Wakana's talent for sewing and crafting. Impressed by his skills, she boldly asks him to help her create her cosplay costumes. Though initially hesitant and overwhelmed by Marin's boldness, Wakana agrees to help her. This marks the beginning of an unexpected friendship."
+    },
+    {
+        id: 5,
+        content: "As they work together, Marin and Wakana embark on a series of creative projects, each costume challenging their skills and deepening their bond. Through these collaborative efforts, Wakana begins to emerge from his shell, gaining confidence and discovering the joy of sharing his passion with someone who genuinely appreciates it. Marin, on the other hand, learns the value of dedication and the intricacies of the crafting process."
+    },
+    {
+        id: 6,
+        content: "Their partnership is not just about cosplay; it becomes a journey of mutual growth. Marin's infectious enthusiasm and supportive nature help Wakana confront his insecurities, while Wakana's meticulous craftsmanship and patience teach Marin the importance of effort and attention to detail."
+    },
+    {
+        id: 7,
+        content: "Romantic feelings gradually blossom between them, adding another layer of complexity to their relationship. Their dynamic is a blend of heartfelt moments, comedic interactions, and genuine emotional support, making their story both relatable and heartwarming."
+    },
+    {
+        id: 8,
+        content: "My Dress-Up Darling beautifully captures the essence of finding one's place in the world through shared passions and the courage to be oneself. It highlights how two seemingly different individuals can come together, inspire each other, and create something truly special. The anime is a celebration of creativity, friendship, and the blossoming of young love, all set against the colorful backdrop of cosplay culture."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+export const RelatedMangaLinks = [
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran',
+        link: 'https://www.frieranmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/one-punch-man.webp`,
+        title: 'One Punch Man',
+        link: 'https://www.onepunchmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/vinland-saga.webp`,
+        title: 'Vinland Saga',
+        link: 'https://www.vinlandsagamanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/jujutsu-kaisen.webp`,
+        title: 'Jujutsu Kaisen',
+        link: 'https://www.jujutsukaisenmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/black-clover.webp`,
+        title: 'Black Clover',
+        link: 'https://www.blackclovermanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/chainsaw-man.webp`,
+        title: 'Chainsaw Man',
+        link: 'https://www.chainsawmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/mushoku-tensei.webp`,
+        title: 'Mushoku Tensei',
+        link: 'https://www.mushokutenseimanga.in.net',
+    },
+];
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "7a",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "15a",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "90a",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "95",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "96",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "97",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "98",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "99",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "100",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "101",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "102",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "103",
+        "numImages": 20
+    }
+];
